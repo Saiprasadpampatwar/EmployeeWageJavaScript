f3283858c4bd911cbfe05e79a7c7831e71e15a38
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-29 11:53:58 +0530

    [Saiprasad]Added: Ability to check the name with given regex pattern

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,56 +1,73 @@
 class EmployeePayrollData {
     // property
     id;
-    name;
     salary;
     gender;
     startDate;
-
+  
     constructor(...params) {
-        this.id = params[0];
-        this.name = params[1];
-        this.salary = params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
+      this.id = params[0];
+      this.name = params[1];
+      this.salary = params[2];
+      this.gender = params[3];
+      this.startDate = params[4];
     }
-
+  
+    // getter and setter method
     get name() {
-        return this._name;
+      return this._name;
     }
-    
+  
     set name(name) {
-    this.name = name;
+      let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+      if (nameRegex.test(name)) {
+        this._name = name;
+      } else throw "Name is Incorrect!";
     }
-
+  
+    // method
     toString() {
-        const options = {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        };
-        const empDate =
-          this.startDate == undefined
-            ? "undefined"
-            : this.startDate.toLocaleDateString("en-US", options);
-        return (
-          "id=" +
-          this.id +
-          ", name=" +
-          this.name +
-          ", salary=" +
-          this.salary +
-          ", gender=" +
-          this.gender +
-          ", startdate=" +
-          this.startDate
-        );
-      }
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      const empDate =
+        this.startDate == undefined
+          ? "undefined"
+          : this.startDate.toLocaleDateString("en-US", options);
+      return (
+        "id=" +
+        this.id +
+        ", name=" +
+        this.name +
+        ", salary=" +
+        this.salary +
+        ", gender=" +
+        this.gender +
+        ", startdate=" +
+        this.empDate
+      );
     }
-
-
-let employeePayrollData = new EmployeePayrollData(1,"Mark",300000);
-console.log(employeePayrollData.toString());
-employeePayrollData.name = "john";
-console.log(employeePayrollData.toString());
-let employeePayrollData1 = new EmployeePayrollData(1,"Terrisa",300000,'F',new Date());
-console.log(employeePayrollData1.toString());
+  }
+  
+  let employeePayrollData = new EmployeePayrollData(1, "Mark", 20000);
+  console.log(employeePayrollData.toString());
+  employeePayrollData.id = 0;
+  employeePayrollData.name = "Jeff";
+  console.log(employeePayrollData.toString());
+  
+  try {
+    //employeePayrollData.name = '';
+    employeePayrollData = new EmployeePayrollData(
+      1,
+      "mark",
+      20000,
+      "M",
+      new Date()
+    );
+    console.log(employeePayrollData.toString());
+  } catch (e) {
+    console.error(e);
+  }
+  console.log(employeePayrollData.toString());
